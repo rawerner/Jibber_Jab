fe0d149f83b1e93e062ddb585ac64e476248e6d9
Rachel Werner <rachelannwerner@gmail.com>
2014-02-18 13:49:24 -0600

    Initial commit and Update Gemfile.lock for Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JibberJab::Application.config.secret_key_base = 'd155161c4f7adaa427c0b0e3944f83f1d0311b55042578c85f0e9d8cd776c746c28697f887cf667b230f689680d50c1bdcec9456e33aa50e551e09129fafb390'
